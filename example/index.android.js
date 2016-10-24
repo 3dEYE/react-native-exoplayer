e744657c6d99c06e6996c04e566f2503643ae456
lemming <alexey@tcherevatov.ru>
2016-10-24 03:50:58 +0300

    update example

--- example/index.android.js
+++ example/index.android.js
@@ -6,68 +6,61 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity
 } from 'react-native';
 
 import Video from 'react-native-3deye-video';
 
 export default class VideoPlayer extends Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+	}
 
-    this.state = {
-      url: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'
-    };
-  }
-
-  render() {
-    const { url } = this.state;
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <View>
-          <Video
-            style={{width: 320, height: 240}}
-            file={url}
-          />
-        </View>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.playerContainer}>
+					<View style={styles.player}>
+						<Video
+							style={{width: 320, height: 240}}
+							file={'http://ios-streaming.cnet.com/mpx/hls/2016/08/19/747447363521/Baby_Monitor_Capsule_892578_800/Baby_Monitor_Capsule_892578_800.m3u8'}
+						/>
+					</View>
+					<View style={styles.control}></View>
+				</View>
+				<View style={styles.playerContainer}>
+					<View style={styles.player}>
+						<Video
+							style={{width: 320, height: 240}}
+							file={'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'}
+						/>
+					</View>
+					<View style={styles.control}></View>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#F5FCFF'
+	},
+	playerContainer: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+	player: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1
+	}
 });
 
 AppRegistry.registerComponent('VideoPlayer', () => VideoPlayer);
