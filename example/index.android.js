fd876f85ef5776ec42387298a1b1043303f838a9
lemming <alexey@tcherevatov.ru>
2016-10-24 20:15:10 +0300

    add one more test video to example

--- example/index.android.js
+++ example/index.android.js
@@ -1,12 +1,12 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import React, { Component } from 'react';
 import {
-	AppRegistry,---
+	AppRegistry,
 	StyleSheet,
 	Text,
 	View,
@@ -18,58 +18,56 @@ import Video from 'react-native-3deye-video';
 export default class VideoPlayer extends Component {
 
 
-  constructor(props) {
-    super(props);
- 	  this._onEnd = this._onEnd.bind(this); 
- 	  this._onProgress = this._onProgress.bind(this);
-    this._onError = this._onError.bind(this); 
-    this.state = {
-      url: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'
-    };
-  }
+	constructor(props) {
+		super(props);
+		this._onEnd = this._onEnd.bind(this);
+		this._onProgress = this._onProgress.bind(this);
+		this._onError = this._onError.bind(this);
+		this.state = {
+			url: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'
+		};
+	}
 
-  _onEnd(){
-  	alert('end');
-  }
+	_onEnd(){
+		alert('end');
+	}
 
-  _onError(data){
-  	alert('error');
-  }
+	_onError(data){
+		alert('error');
+	}
 
-  _onProgress(data){
-  	alert('progress: duration = '+ data.duration +" current time = " + data.currentTime);
-  }
+	_onProgress(data){
+		alert('progress: duration = '+ data.duration +" current time = " + data.currentTime);
+	}
 
-  render() {
-    const { url } = this.state;
-     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!!!{this.state.text}
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <View>
-          <Video
-            style={{width: 320, height: 240}}
-            source={url}
-            onEnd={this._onEnd}
-            onError={this._onError}
-            onProgress={this._onProgress}
-            /*rate={1.0}
-            volume={1.0}
-            muted={false}
-            paused={true}*/
-          />
-        </View>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		const { url } = this.state;
+		return (
+			<View style={styles.container}>
+				<View style={styles.playerContainer}>
+					<View style={styles.player}>
+						<Video
+							style={{width: 320, height: 180}}
+							source={'http://ios-streaming.cnet.com/mpx/hls/2016/08/19/747447363521/Baby_Monitor_Capsule_892578_800/Baby_Monitor_Capsule_892578_800.m3u8'}
+							onEnd={this._onEnd}
+							onError={this._onError}
+							onProgress={this._onProgress}
+							/>
+					</View>
+					<View style={styles.control}></View>
+				</View>
+				<View style={styles.playerContainer}>
+					<View style={styles.player}>
+						<Video
+							style={{width: 320, height: 240}}
+							source={'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'}
+							/>
+					</View>
+					<View style={styles.control}></View>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
