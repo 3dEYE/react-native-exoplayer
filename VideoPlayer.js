0b517aa4990d6994b28673ea339b6c13a6dbfb6d
lemming <alexey@tcherevatov.ru>
2016-10-24 21:36:41 +0300

    fix events passing from native to React on Android

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1,61 +1,60 @@
-import { PropTypes } from 'react';
+import { PropTypes, Component } from 'react';
 import { requireNativeComponent, View } from 'react-native';
-var React = require('react');
 
-export default class Video extends React.Component { 
-  
-  setNativeProps(nativeProps) {
-    this._root.setNativeProps(nativeProps);
-  }
-  
+export default class Video extends Component {
+
+	setNativeProps(nativeProps) {
+		this._root.setNativeProps(nativeProps);
+	}
+
 	_onError = (event:Event) => {
-  	if (this.props.onError) {
-   		this.props.onError(event.nativeEvent);
-   	}
- 	};
-
-  _assignRoot = (component) => {
-    this._root = component;
-  };
-
- 	_onProgress = (event:Event) => {
-   	if (this.props.onProgress) {
-   		this.props.onProgress(event.nativeEvent);
-   	}
- 	};
-
- 	_onEnd = (event:Event) => {
-  	if (this.props.onEnd) {
-   		this.props.onEnd(event.nativeEvent);
-   	}
+		if (this.props.onError) {
+			this.props.onError(event.nativeEvent);
+		}
 	};
 
-	render() { 
-    const nativeProps = Object.assign({}, this.props);
-    Object.assign(nativeProps, {
-       onError:this._onError,
-       onProgress : this._onProgress,
-       onEnd : this._onEnd,
-    });
+	_assignRoot = (component) => {
+		this._root = component;
+	};
+
+	_onProgress = (event:Event) => {
+		if (this.props.onProgress) {
+			this.props.onProgress(event.nativeEvent);
+		}
+	};
+
+	_onEnd = (event:Event) => {
+		if (this.props.onEnd) {
+			this.props.onEnd(event.nativeEvent);
+		}
+	};
+
+	render() {
+		const nativeProps = Object.assign({}, this.props);
+		Object.assign(nativeProps, {
+			onError:this._onError,
+			onProgress : this._onProgress,
+			onEnd : this._onEnd,
+		});
 		return (
-      <RNExoPlayer
-			  ref={this._assignRoot}
-        {...nativeProps}
-      />
-    );
+			<RNExoPlayer
+				ref={this._assignRoot}
+				{...nativeProps}
+				/>
+		);
 	}
 }
 
-Video.propTypes = { 
-   	source: PropTypes.string,
-    rate: PropTypes.number,
-    volume: PropTypes.number,
-    paused: PropTypes.bool,
-	  muted: PropTypes.bool,
-	  onProgress: PropTypes.func,
-  	onError: PropTypes.func,
-	  onEnd: PropTypes.func,
-    ...View.propTypes // include the default view properties
+Video.propTypes = {
+	source: PropTypes.string,
+	rate: PropTypes.number,
+	volume: PropTypes.number,
+	paused: PropTypes.bool,
+	muted: PropTypes.bool,
+	onProgress: PropTypes.func,
+	onError: PropTypes.func,
+	onEnd: PropTypes.func,
+	...View.propTypes // include the default view properties
 };
 
 const RNExoPlayer = requireNativeComponent(`RNExoPlayer`, Video);
--- example/index.android.js
+++ example/index.android.js
@@ -1,8 +1,8 @@
 /**
-* Sample React Native App
-* https://github.com/facebook/react-native
-* @flow
-*/
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
 import React, { Component } from 'react';
 import {
