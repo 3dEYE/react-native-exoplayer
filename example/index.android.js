9fab61803f2552708316556b3fed5b238fc6fa2e
lemming <alexey@tcherevatov.ru>
2016-11-01 00:55:51 +0300

    update example

--- example/index.android.js
+++ example/index.android.js
@@ -9,49 +9,29 @@ import {
 	AppRegistry,
 	StyleSheet,
 	Text,
-	View,
-	TouchableOpacity
+	View
 } from 'react-native';
+import { RNEP } from 'react-native-3deye-video';
+import PlayerView from './js/PlayerView';
 
-import { RNEP, Video } from 'react-native-3deye-video';
+import data from './data.json';
+
 
 export default class VideoPlayer extends Component {
 
-
-	constructor(props) {
-		super(props);
-		this._onEnd = this._onEnd.bind(this);
-		this._onProgress = this._onProgress.bind(this);
-		this._onError = this._onError.bind(this);
-		this.state = {
-			url: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8',
-			volume:0.1,
-			muted:false,
-			paused:false,
-			speed:1.0
-		};
-	}
-
-	_onEnd(){
-		alert('end');
-
-	}
-
-	_onError(data){
-		alert('error '+data.error);
-	}
-
-	_onProgress(data){
-		//alert('progress: duration = '+ data.duration +" current time = " + data.currentTime);
+	componentDidMount() {
+		//RNEP.isRateSupported().then((result) => { alert("Change of speed is supported: " + result);});
+		// RNEP.getMaxSupportedVideoPlayersCount("Calculation")
+		// 	.then((result) => {alert(" max= "+result.maxSupported+" heap="+result.heapSize+" Mb")});
 	}
 
 	render() {
-		const { url } = this.state;
-		//RNEP.isRateSupported().then((result) => { alert("Change of speed is supported: " + result);});
-		RNEP.getMaxSupportedVideoPlayersCount("Calculation")
-		.then((result) => {alert(" max= "+result.maxSupported+" heap="+result.heapSize+" Mb")});
+		const players = data.map((props, idx) => <PlayerView key={idx} {...props} />);
+
 		return (
-			<View/>
+			<View style={styles.container}>
+				{players}
+			</View>
 		);
 
 	}
@@ -60,16 +40,8 @@ export default class VideoPlayer extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#F5FCFF'
-	},
-	playerContainer: {
-		flex: 1,
-		flexDirection: 'column'
-	},
-	player: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1
+		backgroundColor: '#F5FCFF',
+		padding: 48
 	}
 });
 
